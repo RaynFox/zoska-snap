74028c4e75070663d9a08437182155469216f889
RaynFox <alex.sklar2006@gmail.com>
2024-09-26 12:34:56 +0200

    Initial commit of zoska-snap

---
+++ src/app/gdpr/page.tsx
@@ -0,0 +1,7 @@
+import Typography from "@mui/material/Typography";
+
+export default function GDPR() {
+  return (
+    <Typography> Ochrana osobných údajov </Typography>
+  );
+}
---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,7 @@
+import Typography from "@mui/material/Typography";
+
+export default function AboutMe() {
+  return (
+    <Typography> Stránka o mne </Typography>
+  );
+}
